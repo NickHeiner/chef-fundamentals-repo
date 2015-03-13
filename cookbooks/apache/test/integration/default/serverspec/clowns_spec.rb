48dd12a023b2c2dc8f2e7a533673abae9ac154ad
Nick Heiner <nick.heiner@opower.com>
2015-03-13 13:39:11 -0400

    adding more tests

--- cookbooks/apache/test/integration/default/serverspec/clowns_spec.rb
+++ cookbooks/apache/test/integration/default/serverspec/clowns_spec.rb
@@ -4,4 +4,8 @@ describe 'clowns site' do
     it 'responds on port 80' do
         expect(port 80).to be_listening 'tcp'
     end
+
+    it 'talks about clowns' do
+        expect((command 'curl -s localhost:80').stdout).to match /clowns/
+    end
 end
